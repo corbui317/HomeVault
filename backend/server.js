51def9366a16178cb36ed5feb9df6f59dc978032
Corey Bui <corbui317@gmail.com>
2025-06-22 00:39:19 -0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
+const bcrypt = require('bcryptjs');
+const User = require('./models/User');
 
 // Connect to MongoDB
 connectDB();
+
+// Create default admin user when it doesn't exist
+async function ensureAdmin() {
+  const existing = await User.findOne({ username: 'admin' });
+  if (!existing) {
+    const hashed = await bcrypt.hash('password', 10);
+    await User.create({ username: 'admin', password: hashed });
+    console.log('Default admin user created');
+  }
+}
+ensureAdmin();
 
 const app = express();
 const corsOptions = {
